3b3c78095d233708071a84c369fb607bed640607
unipon1124 <2021php41-student034@la-study.com>
2021-05-19 16:32:13 +0900

    マイページ画面を作成しました

---
+++ resources/views/auth/mypage.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>マイページ</h1>
+    <table>
+        <tr>
+            <td><a href="{{ route('') }}">会員情報編集</a></td>
+            <td><a href="{{ route('') }}">過去の予約</a></td>
+            <td><a href="{{ route('') }}">ログアウト</a></td>
+        </tr>
+    </table>
+<h2>会員詳細情報</h2>
+    <table border="1">
+        <tr>
+            <th>名前</th>
+            <th>住所</th>
+            <th>電話番号</th>
+            <th>メールアドレス</th>
+            <th>生年月日</th>
+            <th>パスワード</th>
+        </tr>
+        <tr>
+            <td>{{ Auth::user()->name }}</td>
+            <td>{{ Auth::user()->address }}</td>
+            <td>{{ Auth::user()->tel }}</td>
+            <td>{{ Auth::user()->email }}</td>
+            <td>{{ Auth::user()->birthday }}</td>
+            <td>{{ Auth::user()->password }}</td>
+        </tr>
+    </table>
+<h2>現在の予約一覧</h2>
+    
+    <a href="{{ route('') }}">戻る</a>
+    <a href="{{ route('') }}">退会する</a>    
+@endsection
