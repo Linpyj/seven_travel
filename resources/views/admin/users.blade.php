16ff6404d5e595770fc8828fdd07f04d6fa7c67d
unipon1124 <2021php41-student034@la-study.com>
2021-05-21 09:53:39 +0900

    口コミ投稿画面、口コミ投稿完了画面の作成、会員一覧画面の編集をしました

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -11,4 +11,12 @@
 </p>
 <p><button type="submit">検索</button></p>
 </form>
+@foreach($users as $user)
+    <tr>
+        <td>{{ users()->id }}</td>
+    </tr>
+@endforeach
+
+    {{ $products->appends(Request::all())->links() }}
+
 @endsection
