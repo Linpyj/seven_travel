56aed257f6176dea141d4501f273f4c2200dbacf
unipon1124 <2021php41-student034@la-study.com>
2021-05-21 11:24:52 +0900

    スペース消しました！

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -10,7 +10,7 @@
 @foreach($users as $user)
 <!-- 検索結果を連想配列として一つずつ取り出して表示 -->
     <tr>
-        <td><a href="{{ route ('admin.users', $user->id) }}">{{ $user->id }}</a></td>
+        <td><a href="{{ route('admin.users', $user->id) }}">{{ $user->id }}</a></td>
     </tr>
 @endforeach
 {{ $products->appends(Request::all())->links() }}
