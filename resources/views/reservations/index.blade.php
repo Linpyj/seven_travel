e01c520b166589d80ef2c37a199fc4dab756af3a
Takuma Tanaka <2021php41-student003@la-studey.com>
2021-05-24 11:53:14 +0900

    口コミ投稿機能作成

--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -6,7 +6,12 @@
 @foreach ($reservations as $reservation)
     <h2>{{ $reservation->plan->hotel->name }}：{{ $reservation->plan->name }}</h2>
     <p>チェックイン時間{{ $reservation->plan->hotel->check_in }}チェックアウト時間{{ $reservation->plan->hotel->check_out }}</p>
-    <a href="{{ route('reviews.create', $reservation) }}">口コミを投稿する</a>
+    <form action="{{ route('reviews.create') }}" method="get">
+        @csrf
+        <input type="hidden" name="hotel_id" value="{{ $reservation->plan->hotel_id }}">
+        <button type="submit">口コミを投稿する</button>
+    </form>
+    {{-- <a href="{{ route('reviews.create', $reservation) }}">口コミを投稿する</a> --}}
     <a href="{{ route('plans.show', $reservation) }}">再予約する</a>
 @endforeach
 <a href="{{ route('users.index') }}">戻る</a>
