080424e1ff9e0e408534863b1e8063d76796d154
Takuma Tanaka <2021php41-student003@la-studey.com>
2021-05-24 13:10:07 +0900

    予約削除機能

--- resources/views/auth/mypage.blade.php
+++ resources/views/auth/mypage.blade.php
@@ -42,7 +42,20 @@
 <h2>現在の予約一覧</h2>
     @foreach ($reservations as $reservation)
         <h2>{{ $reservation->plan->hotel->name }}：{{ $reservation->plan->name }}</h2>
-        <p>チェックイン時間{{ $reservation->plan->hotel->check_in }}チェックアウト時間{{ $reservation->plan->hotel->check_out }}</p>
+        <p>チェックイン日{{ $reservation->check_in }}チェックアウト日{{ $reservation->check_out }}</p>
+        <a href="" onclick="reservation_destroy()">予約キャンセル</a>
+        <form action="{{ route('reservations.destroy', $reservation->id) }}" id="reservation-destroy-form" method="post">
+            @csrf
+            @method('delete')
+        </form>
+        <script type="text/javascript">
+            function reservation_destroy() {
+                event.preventDefault();
+                if(window.confirm('本当に予約をキャンセルしてもよろしいですか？')){
+                    document.getElementById('reservation-destroy-form').submit();
+                }
+            }
+        </script>
     @endforeach
     <a href="{{ route('home') }}">戻る</a>
     <a href="{{ route('users.destroy', \Auth::id()) }}" onclick="deleteUser()">退会する</a>
