3698ae68faa0db267b8f46afcd4521f4bebe2e90
unipon1124 <2021php41-student034@la-study.com>
2021-05-25 10:24:53 +0900

    管理者画面を編集しました

--- resources/views/hotels/index.blade.php
+++ resources/views/hotels/index.blade.php
@@ -1,13 +1,26 @@
 @extends('layouts.app')
 @section('content')
-<div class="top">
-    <h1 class="title">SEVEN TRAVEL</h1>
-    <p class="copy">行きたい、がきっと見つかる。</p>
-    <p>This is sample texts.This is sample texts.This is sample texts.This is sample texts.</p>
-</div>
-<p class="subtitle">SEVEN TRAVELおすすめのホテル</p>
-<p id="catch">SEVEN TRAVEL厳選！各シーズンおすすめホテル</p>
-<a href="/"><img src="/sample1.jpg" alt="spring_hotel" class="top_image"></a>
-<a href="/"><img src="/sample1.jpg" alt="fall_hotel" class="top_image"></a>
-<a href="/"><img src="/sample1.jpg" alt="winter_hotel" class="top_image"></a>
+@if(Auth::user()->is_admin)
+    <h1>ホテル一覧</h1>
+    <form action="{{route('hotels.index')}}" method="get">
+    @csrf
+    @include('hotels.search')
+    <button type="submit">検索</button>
+    </form>
+    @foreach($hotels as $hotel)
+    
+    @endforeach
+    <!-- {{ $products->appends(Request::all())->links() }} -->
+@elseif(Auth::user())
+    <div class="top">
+        <h1 class="title">SEVEN TRAVEL</h1>
+        <p class="copy">行きたい、がきっと見つかる。</p>
+        <p>This is sample texts.This is sample texts.This is sample texts.This is sample texts.</p>
+    </div>
+    <p class="subtitle">SEVEN TRAVELおすすめのホテル</p>
+    <p id="catch">SEVEN TRAVEL厳選！各シーズンおすすめホテル</p>
+    <a href="/"><img src="/sample1.jpg" alt="spring_hotel" class="top_image"></a>
+    <a href="/"><img src="/sample1.jpg" alt="fall_hotel" class="top_image"></a>
+    <a href="/"><img src="/sample1.jpg" alt="winter_hotel" class="top_image"></a>
+@endif
 @endsection
