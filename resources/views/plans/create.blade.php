141361cd49cf7ce7e2ef0d131eeec4e6b5aa30a7
Rintaro Fujii <2021php41-student024@la-study.com>
2021-05-25 17:06:36 +0900

    reservations/createとplans/createのplaceholderを追加

--- resources/views/plans/create.blade.php
+++ resources/views/plans/create.blade.php
@@ -9,22 +9,22 @@
         <dl>
         <dt>プランの名前</dt>
         <dd>
-            <input type="text" name="name" value="{{ old('name', $plan->name) }}">
+            <input type="text" name="name" value="{{ old('name', $plan->name) }}" placeholder="50文字以内で入力してください。">
         </dd>
         
         <dt>プランの価格</dt>
         <dd>
-            <input type="" name="price" value="{{ old('price', $plan->price)}}">
+            <input type="" name="price" value="{{ old('price', $plan->price)}}" placeholder="7桁以内で入力してください。">
         </dd>
 
         <dt>プランの部屋数</dt>
         <dd>
-            <input type="text" name="number_of_room" value="{{ old('number_of_room', $plan->number_of_room)}}">
+            <input type="text" name="number_of_room" value="{{ old('number_of_room', $plan->number_of_room)}}" placeholder="4桁以内で入力してください。">
         </dd>
     
         <dt>見どころ</dt>
         <dd>
-            <textarea name="remark" row="5">{{ old('remark', $plan->remark)}}</textarea>
+            <textarea name="remark" row="5" placeholder="100文字以内で入力してください。">{{ old('remark', $plan->remark)}}</textarea>
         </dd>
         </dl>
     
--- resources/views/reservations/create.blade.php
+++ resources/views/reservations/create.blade.php
@@ -10,18 +10,18 @@
     <dd><a href="{{ route('hotels.show', $plan->hotel->id) }}">{{ $plan->hotel->name }}</a></dd>
 
     <p>上記のホテルとプラン名で予約します。</p>
-    <p>氏名とチェックイン日、チェックアウト日を入力してください。</p>
+    <p>チェックイン日、チェックアウト日を入力してください。</p>
 
     <form action="{{ route('confirm') }}" method="post">
         @csrf
         <div>
             <label>チェックイン日</label>
-            <input type="date" name="check_in" value="{{ old('check_in') }}">
+            <input type="date" name="check_in" value="{{ old('check_in') }}" placeholder="チェックイン日は入力必須です。">
         </div>
 
         <div>
             <label>チェックアウト日</label>
-            <input type="date" name="check_out" value="{{ old('check_out') }}"></label>
+            <input type="date" name="check_out" value="{{ old('check_out') }}" placeholder="チェックアウト日は入力必須です。"></label>
         </div>
         <div>
             <input type="hidden" name="plan_id" value="{{ $plan->id }}">
