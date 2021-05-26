d51ffe13288b3fad6f1b316c39a9376d62f9a770
Rintaro Fujii <2021php41-student024@la-study.com>
2021-05-26 10:35:51 +0900

    予約履歴がない時の表示を修正

--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -5,22 +5,26 @@
 <h1>過去の予約</h1>
 
 <div class="old-form">
-@foreach ($reservations as $reservation)
-    <h2>{{ $reservation->plan->hotel->name }}：{{ $reservation->plan->name }}</h2>
-    <p class="script">チェックイン日{{ $reservation->check_in }}　チェックアウト日{{ $reservation->check_out }}</p>
-    <form action="{{ route('reviews.create') }}" method="get">
-        @csrf
-        <input type="hidden" name="hotel_id" value="{{ $reservation->plan->hotel_id }}">
-        <section>
-            <button type="submit" class="btn_2"><span>口コミを投稿する</span></button>
-        </section>
-    </form>
-    {{-- <a href="{{ route('reviews.create', $reservation) }}">口コミを投稿する</a> --}}
+@if (count($reservations) != 0)
+    @foreach ($reservations as $reservation)
+        <h2>{{ $reservation->plan->hotel->name }}：{{ $reservation->plan->name }}</h2>
+        <p class="script">チェックイン日{{ $reservation->check_in }}　チェックアウト日{{ $reservation->check_out }}</p>
+        <form action="{{ route('reviews.create') }}" method="get">
+            @csrf
+            <input type="hidden" name="hotel_id" value="{{ $reservation->plan->hotel_id }}">
+            <section>
+                <button type="submit" class="btn_2"><span>口コミを投稿する</span></button>
+            </section>
+        </form>
+        {{-- <a href="{{ route('reviews.create', $reservation) }}">口コミを投稿する</a> --}}
 
-    <section>
-        <a href="{{ route('plans.show', $reservation) }}" class="btn_1"><span>再予約する</span></a>
-    </section>
-@endforeach
+        <section>
+            <a href="{{ route('plans.show', $reservation) }}" class="btn_1"><span>再予約する</span></a>
+        </section>
+    @endforeach
+@else
+    <p style="text-align: center;">予約履歴がありません。</p>
+@endif
 </div>
 
 <br>
